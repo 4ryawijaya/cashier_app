3b50e391001dfca3d0dc87173b7ebe48718cb39b
putu_aryawijaya <putu.aryawijaya@manuva.com>
2023-03-30 00:26:28 +0700

    testing integrating display

--- main.py
+++ main.py
@@ -2,3 +2,19 @@
 #fungsi input item
 #fungsi input quantity
 #fungsi input price
+from function import Operation
+from display import Display
+
+while True:
+    Display.opening()
+    item_name = input('Enter item name (or enter "done" to finish): ')
+    if item_name == 'done':
+        Display.closing()
+        break
+
+# idVAR = f"Your transaction id is {random_digits(5)}"
+# itemVAR = input("Please input your item:")
+# qtyVAR = input("Please item quantity : ")
+
+# test1 = Operation(idVAR, itemVAR, qtyVAR)
+
