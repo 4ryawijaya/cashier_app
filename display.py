a4c94359ca6a07bd9bb123bf7a8112378ece4742
putu_aryawijaya <putu.aryawijaya@manuva.com>
2023-04-03 12:29:19 +0700

    update display

--- display.py
+++ display.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from function import ItemOperation, TransactionId
 
 CURRENT_DATETIME = datetime.now().strftime("%Y-%m-%d")
+ID_TRANSAKSI = TransactionId.transaction_id
 
 class Display:
     def opening():
@@ -13,6 +15,27 @@ class Display:
               """)
         
     def transaction_display():
+        print(f"""
+            ID Transaksi: {ID_TRANSAKSI} 
+            """)
+    
+    def transaction_choice():
+        print("""
+              Untuk Melanjutkan Transaksi Silahkan Pilih menu berikut:
+              1. Add Item               | 5. Delete Item
+              2. Update Item Name       | 6. Reset Transaction
+              3. Update Item Quantity   | 7. Check Order
+              4. Update Item Price      | 8. Check Out
+              """)
+        
+    def item_display():
+        print(f"""
+            ====================== ITEM LIST =======================                   
+            ========================================================
+            
+            """)
+        
+    def checkout_display():
         pass
     
     def closing():
