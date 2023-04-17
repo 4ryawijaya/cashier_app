396a0239cdc6d59bbf47948109e0cf459d2ab8e6
putu_aryawijaya <putu.aryawijaya@manuva.com>
2023-04-17 22:30:13 +0700

    cleaning display code

--- display.py
+++ display.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from function import ItemOperation
 
 CURRENT_DATETIME = datetime.now().strftime("%Y-%m-%d")
 
@@ -12,37 +11,7 @@ class Display:
             Dapatkan diskon untuk pembelian dengan nominal tertentu.
             Selamat berbelanja!
               """)
-    
-    def transaction_choice():
-        print("""
-              Untuk Melanjutkan Transaksi Silahkan Pilih menu berikut:
-              1. Add Item               | 5. Delete Item
-              2. Update Item Name       | 6. Reset Transaction
-              3. Update Item Quantity   | 7. Check Order
-              4. Update Item Price      | 8. Check Out
-              """)
-        
-        def operasi(): 
-            pilih = input(int("Pilih nomor: "))
-            if pilih == 1:
-                print("Silahkan menambahkan item anda: ")
-                ItemOperation.add_item()
-            elif pilih == 2:
-                print("Silahkan ubah nama item anda.")
-            elif pilih == 3:
-                print("Silahkan ubah kuantitas item anda.")
-            elif pilih == 6:
-                print("Berikut daftar belanja anda")
-                ItemOperation.check_order()
-            elif pilih == 8:
-                pass
-            else:
-                print("Pilihan anda di luar jangkauan")
-
-            
-            operasi()
-            
-        
+           
         
     def item_display():
         print(f"""
@@ -50,9 +19,7 @@ class Display:
             ========================================================
             
             """)
-        
-    def checkout_display():
-        pass
+
     
     def closing():
        print(f"""
