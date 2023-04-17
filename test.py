8c8b66f1f960730f4aecd8752cc6eb3ba2200246
putu_aryawijaya <putu.aryawijaya@manuva.com>
2023-04-17 22:04:50 +0700

    learn to input items to dictionary

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
+# class Client:
+#     def __init__(self, name):
+#         self.name = name
+        
+#     def add_name(self):
+#         self.name = input("What is band's name")
+#         return self.name
+        
+#     def print_info(self):
+#         print(f"{self.name}")
+        
+# client = Client.add_name()
+# client.print_info()
+
 client_info = {} # start empty
 
 while True:
@@ -8,3 +22,9 @@ while True:
     client_info[new_client_id] = new_client
 
     print("client added, client_info is now", client_info)
+
+
+    
+
+        
+    
