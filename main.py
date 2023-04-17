5cab1a979d5a89af4d03592330967c2c097d17e3
putu_aryawijaya <putu.aryawijaya@manuva.com>
2023-04-17 22:38:14 +0700

    set up choice, import function, import display

--- main.py
+++ main.py
@@ -1,57 +1,71 @@
-def run(self):
+from function import ItemManager
+from display import Display
+import time
+
+def run():
         # Main loop to receive input and operate methods until checkout
         while True:
+            Display.opening()
+            time.sleep(5)
+            
             print("Choose an option:")
             print("1. Add item")
             print("2. Display last input")
             print("3. Display all inputs")
             print("4. Checkout")
             print("5. Reset inputs")
-            print("6. Exit")
+            print("6. Edit Item Name")
+            print("7. Edit Item Quantity")
+            print("8. Edit Item Price")
+            print("9. Exit Transaction")
             choice = input("Enter your choice: ")
 
             if choice == '1':
                 item_name = input("Enter item name: ")
                 item_price = float(input("Enter item price: "))
                 item_quantity = int(input("Enter item quantity: "))
-                self.addItem(item_name, item_price, item_quantity)
+                ItemManager.addItem(item_name, item_price, item_quantity)
                 print("Item added.")
                 print()
 
             elif choice == '2':
-                self.displayLastInput()
+                Display.item_display()
+                ItemManager.displayLastInput()
 
             elif choice == '3':
-                self.displayAllInputs()
+                Display.item_display()
+                ItemManager.displayAllInputs()
 
             elif choice == '4':
-                self.checkout()
+                ItemManager.checkout()
                 break  # Exit loop after checkout
 
             elif choice == '5':
-                self.resetInputs()
+                ItemManager.resetInputs()
                 print("Inputs reset.")
                 print()
 
             elif choice == '6':
                 index = int(input("Enter item index to edit: "))
                 new_name = input("Enter new item name: ")
-                self.editItemName(index, new_name)
+                ItemManager.editItemName(index, new_name)
             
             elif choice == '7':
                 index = int(input("Enter item index to edit: "))
                 new_quantity = int(input("Enter new item quantity: "))
-                self.editItemQuantity(index, new_quantity)
+                ItemManager.editItemQuantity(index, new_quantity)
 
             elif choice == '8':
                 index = int(input("Enter item index to edit: "))
                 new_price = float(input("Enter new item price: "))
-                self.editItemPrice(index, new_price)
+                ItemManager.editItemPrice(index, new_price)
 
             elif choice == '9':
-                print("Goodbye!")
+                Display.closing()
                 break  # Exit loop and end script
 
             else:
                 print("Invalid choice. Try again.")
                 print()
+
+run()
